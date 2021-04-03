2991dc5d49fe6b0bcd965f35192ab0c88641199c
Jamie Blaut <jagoda.blaut@bbc.co.uk>
2021-04-03 20:45:18 +0100

    make server file easier to read

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ const payments = require('./routes/payments');
 const history = require('./routes/history');
 const connectDB = require('./config/db')
 
-const app = express();
-app.use(express.json())
-
 dotenv.config({ path: './config/config.env'});
 connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json())
 app.use('/api/v1/payments', payments);
 app.use('/api/v1/history', history);
 
-const PORT = process.env.PORT || 4000;
-
 app.listen(PORT, console.log(`Server running on ${PORT}`));
-
