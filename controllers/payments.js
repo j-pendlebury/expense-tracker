6c8c3abdb338f81bbe2692f74bf48fa4d648ab44
Jamie <j_blaut@hotmail.com>
2021-04-03 17:57:02 +0100

    why cant i spell response????

--- controllers/payments.js
+++ controllers/payments.js
@@ -1,6 +1,6 @@
 const Payment = require('../models/Payment');
 
-const resposeObj = (success, responseCode, results = {}, error = '') => {
+const responseObj = (success, responseCode, results = {}, error = '') => {
   const defaultResponse = {
     success,
     responseCode
@@ -20,9 +20,9 @@ exports.getPayments = async (req, res, next) => {
   try {
     const payments = await Payment.find();
     
-    return res.status(200).json(resposeObj(true, 200, payments));
+    return res.status(200).json(responseObj(true, 200, payments));
   } catch (err) {
-    return res.status(500).json(resposeObj(false, 500, null, err.message))
+    return res.status(500).json(responseObj(false, 500, null, err.message))
   }
 }
 
@@ -32,15 +32,15 @@ exports.addPayment = async (req, res, next) => {
     const { description, amount, person } = req.body;
     const payment = await Payment.create(req.body);
 
-    return res.status(201).json(resposeObj(true, 201, payment))
+    return res.status(201).json(responseObj(true, 201, payment))
   } catch (err) {
     if (err.name === 'ValidationError') {
       const messages = Object.values(err.errors).map(value => value.message);
 
-      return res.status(400).json(resposeObj(false, 400, null, messages))
+      return res.status(400).json(responseObj(false, 400, null, messages))
     }
 
-    return res.status(500).json(resposeObj(false, 500, null, err.message))
+    return res.status(500).json(responseObj(false, 500, null, err.message))
   }
 }
 
@@ -49,12 +49,12 @@ exports.deletePayment = async (req, res, next) => {
   try {
     const payment = await Payment.findById(req.params.id);
 
-    if (!payment) return res.status(404).json(resposeObj(false, 404, null, "No transaction found"));
+    if (!payment) return res.status(404).json(responseObj(false, 404, null, "No transaction found"));
 
     await payment.remove();
 
-    return res.status(200).json(resposeObj(true, 200, {}))
+    return res.status(200).json(responseObj(true, 200, {}))
   } catch (err) {
-    return res.status(500).json(resposeObj(false, 500, null, err.message))
+    return res.status(500).json(responseObj(false, 500, null, err.message))
   }
 }
